38254b254975a8d8e6fe15327ea0c17a3a113b24
root <root@vmi284891.contaboserver.net>
2020-03-05 22:54:43 +0100

    linux update

--- Python container/Websockets/index.py
+++ Python container/Websockets/index.py
@@ -11,7 +11,7 @@ async def hello(websocket, path):
 			greeting = "Print: " + message
 			await websocket.send(greeting)
 
-start_server = websockets.serve(hello, "localhost", 9000)
+start_server = websockets.serve(hello, "0.0.0.0", 9000)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
