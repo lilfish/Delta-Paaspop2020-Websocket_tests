e6c6d96eb3b6208044727d267927041d3ced3d0f
Fish <vincentvenhuizen1997@gmail.com>
2020-03-05 23:16:20 +0100

    update asp core things

--- ASPNC container/websocketTest/Program.cs
+++ ASPNC container/websocketTest/Program.cs
@@ -8,7 +8,7 @@ namespace websocketTest
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             CancellationTokenSource cancellation = new CancellationTokenSource();
 
