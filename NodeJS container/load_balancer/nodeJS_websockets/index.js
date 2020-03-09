6a4509f3128b66a478f9c5e034113f0248fe0f59
Fish <vincentvenhuizen1997@gmail.com>
2020-03-09 13:58:25 +0100

    Updated websocket for more data

--- NodeJS container/load_balancer/nodeJS_websockets/index.js
+++ NodeJS container/load_balancer/nodeJS_websockets/index.js
@@ -1,12 +1,22 @@
 var ws = require("nodejs-websocket")
 
+var connections = 0;
+var messages = 0;
+var connected = 0;
+var disconnected = 0;
+
 var server = ws.createServer(function (conn) {
-	console.log("New connection")
+	connections-=-1;
+	connected-=-1;
+	console.log("connections: " , connections , "; connected: " , connected , "; messages: " , messages, "; disconnected: " , disconnected);
 	conn.on("text", function (str) {
-		console.log("Received "+str)
+		messages-=-1;
+		console.log("connections: " , connections , "; connected: " , connected , "; messages: " , messages, "; disconnected: " , disconnected, "; received: " , str);
 		conn.sendText(str.toUpperCase()+"!!!")
 	})
 	conn.on("close", function (code, reason) {
-		console.log("Connection closed")
+		connections-=1;
+		disconnected-=-1;
+		console.log("connections: " , connections , "; connected: " , connected , "; messages: " , messages, "; disconnected: " , disconnected);
 	})
 }).listen(9000)
