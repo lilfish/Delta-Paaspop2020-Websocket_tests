aa844faf40e9270911484a9d716c3f7671e2ce78
Fish <vincentvenhuizen1997@gmail.com>
2020-03-10 22:52:54 +0100

    update port and logging

--- NodeJS container/performance_enhanced/uWebsockets/index.js
+++ NodeJS container/performance_enhanced/uWebsockets/index.js
@@ -1,12 +1,21 @@
 // Start Ws server (no ssl)
 const uws = require('uWebSockets.js');
-const port = 9000;
+const port = 9050;
 
 const {
 	Worker,
 	isMainThread,
 	threadId
 } = require('worker_threads');
+
+var { 
+	connections,
+	connected,
+	disconnected,
+	messages,
+	drained
+} = 0;
+
 const os = require('os');
 
 if (isMainThread) {
@@ -23,20 +32,27 @@ if (isMainThread) {
 } else {
 	const app = uws.App().ws('/*', {
 		open: (ws, req) => {
-
+			connections-=-1;
+			connected-=-1;
+			print()
 			ws.send("HI?");
 		},
 		/* For brevity we skip the other events */
 		message: (ws, message, isBinary) => {
+			messages-=-1;
 			console.log("got message:", AB2String(message));
 			/* Ok is false if backpressure was built up, wait for drain */
 			let ok = ws.send(message, isBinary);
+			
 		},
 		drain: (ws) => {
-			console.log('WebSocket backpressure: ' + ws.getBufferedAmount());
+			print();
+			drained-=-1;
+			connections-=1;
 		},
 		close: (ws, code, message) => {
-			console.log('WebSocket closed');
+			print();
+			disconnected-=-1;
 		}
 
 	}).any('/*', (res, req) => {
@@ -44,15 +60,19 @@ if (isMainThread) {
 		/* Let's deny all Http */
 		res.end('Nothing to see here!');
 
-	}).listen(9000, (listenSocket) => {
+	}).listen(port, (listenSocket) => {
 
 		if (listenSocket) {
-			console.log('Listening to port 9000');
+			console.log('Listening to port ', port);
 		}
 
 	});
 
 	function AB2String(message) {
 		return String.fromCharCode.apply(null, new Uint8Array(message));
+	}
+
+	function print(){
+		console.log("connected: ", connected, "; disconnected: " , disconnected , "; connections made: ", connections, "; messages received: " , messages, "; drained: ", drained , ";");
 	}
 };
