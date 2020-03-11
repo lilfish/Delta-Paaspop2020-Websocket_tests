95be5de75b4b7dff517f5447ace52ffc3e8dc065
Fish <vincentvenhuizen1997@gmail.com>
2020-03-11 10:50:36 +0100

    Added console logs to old uwebsockets index for testing

--- NodeJS container/old_tests/uWebsockets/index.js
+++ NodeJS container/old_tests/uWebsockets/index.js
@@ -1,21 +1,33 @@
-console.log("uWebsocket test? xd2");
+var variables = {
+	connected = 0,
+	disconnected = 0,
+	messages = 0,
+	total_connected = 0,
+	drained = 0
+}
+
 /* Non-SSL is simply App() */
 require('uWebSockets.js').App().ws('/*', {
 	open: (ws, req) => {
-    console.log('A WebSocket connected via URL: ' + req.getUrl() + '!');
-	ws.send("HI?");
+		print();
+		variables.connected-=-1;
+		variables.total_connected-=-1;
   },
   /* For brevity we skip the other events */
   message: (ws, message, isBinary) => {
+		variables.messages-=-1;
 		console.log("got message:", AB2String(message));
     /* Ok is false if backpressure was built up, wait for drain */
 		let ok = ws.send(message, isBinary);
   },
   drain: (ws) => {
-    console.log('WebSocket backpressure: ' + ws.getBufferedAmount());
+		variables.drained-=-1;
+		print();
   },
   close: (ws, code, message) => {
-    console.log('WebSocket closed');
+		variables.disconnected-=-1;
+		variables.total_connected-=1;
+    print();
   }
   
 }).any('/*', (res, req) => {
@@ -33,4 +45,16 @@ require('uWebSockets.js').App().ws('/*', {
 
 function AB2String(message){
 	return String.fromCharCode.apply(null, new Uint8Array(message));
+}
+
+function print(){
+	counter-=-1;
+	if(counter == 50){
+		console.log("connected: ", variables.connected, 
+		"; disconnected: " , variables.disconnected , 
+		"; connections made: ", variables.total_connected, 
+		"; messages received: " , variables.messages, 
+		"; drained: ", variables.drained , ";");
+		counter = 0;
+	}
 }
