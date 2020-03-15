66fcde3679403179c4167fdeda139c2dbf0336b6
Fish <vincentvenhuizen1997@gmail.com>
2020-03-15 21:48:49 +0100

    Added express with pug, .env and scss
    Added mustard css framework and made a simple login page.

--- docker_compose_test/adminpanel/index.js
+++ docker_compose_test/adminpanel/index.js
@@ -1,35 +1,16 @@
-const path = require('path');
-const express = require('express');
-const vueRenderer = require('@doweb/vuexpress').vueRenderer;
+require('dotenv').config()
+const path = require('path')
+const express = require('express')
+const app = express()
+const port = process.env.PORT;
 
-const app = express();
-
-let options = {
-  views: './views',
-  cache: false,
-  watch: true,
-  onError: (err) => {
-    console.log(err)
-  },
-  onReady: () => {
-    console.log('Ready')
-  }
-};
-
-
-const renderer = vueRenderer(options);
-app.use(renderer);
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'pug')
+app.set('public', path.join(__dirname, 'public'));
+app.locals.basedir = app.get('public');
 
 app.get('/', function (req, res) {
-  res.render('example', {myVar: 'Hello World!'});
-});
+  res.render('index', { title: 'Hey', message: 'Hello there!' });
+})
 
-app.get('/plain', function(req, res) {
-    // render template without html head and body
-    res.render('example', { myVar1: 'my variable one' }, { plain: true, inlineCSS: false });
-});
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
